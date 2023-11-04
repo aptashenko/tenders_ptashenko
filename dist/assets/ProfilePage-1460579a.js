94000388d85b75b6bcaaa5472d547c7b7dd2d672
Artem Ptashenko <aptashenko2019@gmail.com>
2023-11-05 00:00:07 +0200

    changes

--- dist/assets/ProfilePage-1460579a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,a as _}from"./index-945a6258.js";const t={},a={class:"profile-page"};function c(r,s){return o(),_("div",a," profile page ")}const l=e(t,[["render",c]]);export{l as default};
--- dist/assets/useAmountFormat-7c9943da.js
+++
@@ -1,1 +0,0 @@
-const r=()=>({formatAmount:(t,o)=>`${t.toLocaleString("uk-UA",{minimumFractionDigits:2,maximumFractionDigits:2}).replace(".",",")} ${o}`,unformatAmount:(t,o)=>{const n=t.replace(` ${o}`,"");return Number(n)}});export{r as u};
