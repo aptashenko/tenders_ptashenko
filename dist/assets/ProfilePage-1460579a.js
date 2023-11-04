15b88f16661c4c8e2caa5a2dbef5e2515dc5482a
Artem Ptashenko <aptashenko2019@gmail.com>
2023-11-04 23:54:40 +0200

    add dist

---
+++ dist/assets/ProfilePage-1460579a.js
@@ -0,0 +1,1 @@
+import{_ as e,o,a as _}from"./index-945a6258.js";const t={},a={class:"profile-page"};function c(r,s){return o(),_("div",a," profile page ")}const l=e(t,[["render",c]]);export{l as default};
---
+++ dist/assets/Statistic-77ac8458.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,a as s}from"./index-945a6258.js";const c={},_={class:"stats-page"};function a(n,o){return e(),s("div",_," statistic ")}const r=t(c,[["render",a]]);export{r as default};
