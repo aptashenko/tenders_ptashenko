34cc4745faf842206fb45a75a0bd59f19093891f
Artem Ptashenko <aptashenko2019@gmail.com>
2023-11-04 23:45:01 +0200

    add dist

---
+++ dist/assets/DocAnalys-d8a18906.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,a as s}from"./index-93ced3f1.js";const _={},n={class:"doc-analys-page"};function o(a,t){return c(),s("div",n," Аналіз тендерої документації ")}const l=e(_,[["render",o]]);export{l as default};
---
+++ dist/assets/Statistic-f5b0fe56.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,a as s}from"./index-93ced3f1.js";const c={},_={class:"stats-page"};function a(n,o){return e(),s("div",_," statistic ")}const r=t(c,[["render",a]]);export{r as default};
