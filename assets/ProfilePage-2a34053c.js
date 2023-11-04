fd7e610fb35d07390f795e26eb97c75d058d0d29
Artem Ptashenko <aptashenko2019@gmail.com>
2023-11-04 23:54:40 +0200

    add dist

--- assets/ProfilePage-2a34053c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,a as _}from"./index-93ced3f1.js";const t={},a={class:"profile-page"};function c(r,s){return o(),_("div",a," profile page ")}const l=e(t,[["render",c]]);export{l as default};
